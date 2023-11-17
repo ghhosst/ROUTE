638430ce4461cf50c325b38be4cd688ca3cb6f54
ghhosst <kolesnikovvv5@mail.ru>
2023-11-17 21:04:53 +0300

    Partially arrange the menu. Create a ROUTE class with getters and setters.

---
+++ Route/ROUTE.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class ROUTE{
+private:
+	char* NameStart;	// Название начального пункта маршрута
+	char* NameFinish;	// Название конечного пункта маршрута
+	int NumberRoute;	// Номер маршрута
+public:
+	char* GetNameStart();
+	void SetNameStart(char* NameStart);
+	char* GetNameFinish();
+	void SetNameFinish(char* NameFinish);
+	int GetNumberRoute();
+	void SetNumberRoute(int NumberRoute);
+};
